b25e52df1202181fea5a542c51262aec1bdc6e26
23JA1168 <23JA1168@L2377>
2023-10-12 11:46:37 +0100

    adding

---
+++ A104/A104.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace A104
+{
+    internal class A104
+    {
+
+        static void Q1()
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("How many sides on Dice?");
+            int sides = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many rolls would you like?");
+            int rolls = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Output:");
+
+            for (int i = 0; i < rolls; i++)
+            {
+                Console.WriteLine(rnd.Next(sides));
+            }
+        }
+
+        static void Q2()
+        {
+            int total = 0;
+
+            Console.WriteLine("Number?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sum or Factorial?");
+            string choice = Console.ReadLine();
+
+            if (choice == "s")
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    total += i;
+                }
+                Console.WriteLine(total);
+            }
+            else if (choice == "f")
+            {
+                for (int i = 1; i < num; i++)
+                {
+                    num *= i;
+                }
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("incorrect input");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which Question?");
+            int Prog_run = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (Prog_run)
+            {
+                case 1: Q1(); break;
+                case 2: Q2(); break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
