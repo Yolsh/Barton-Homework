1b139acb63e1c739bdc29214e6628b83229770fa
josha <josha@LAPTOP-8L6NCJ6M>
2023-11-08 13:59:13 +0000

    will hw update

--- A107 (Will HW)/A107 (Will HW).cs
+++ A107 (Will HW)/A107 (Will HW).cs
@@ -4,13 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Collections.Specialized;
 
 namespace A107__Will_HW_
 {
     internal class Program
     {
+        static void Q2()
+        {
+            int PlayerOneScore = 0;
+            int PlayerTwoScore = 0;
+            Console.WriteLine("How many games?");
+            int NoOfGamesInMatch = int.Parse(Console.ReadLine());
+            for (int i = 0; i < NoOfGamesInMatch; i++)
+            {
+                Console.WriteLine("Did Player One win the game (enter Y or N)?");
+                string PlayerOneWinsGame = Console.ReadLine();
+                if (PlayerOneWinsGame == "Y")
+                {
+                    PlayerOneScore = PlayerOneScore + 1;
+                }
+                else
+                {
+                    PlayerTwoScore = PlayerTwoScore + 1;
+                }
+
+            }
+            Console.WriteLine(PlayerOneScore);
+            Console.WriteLine(PlayerTwoScore);
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
+            Q2();
+            Environment.Exit(0);
             string x = "true";
             int total = 0;
             string name;
@@ -19,7 +47,7 @@ namespace A107__Will_HW_
             {
                 Console.WriteLine("Enter name");
                 name = Console.ReadLine();
-                Thread.Sleep(1000);
+                Thread.Sleep(200);
                 if (name == "done")
                 {
                     break;
