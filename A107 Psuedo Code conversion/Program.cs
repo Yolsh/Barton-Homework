6a0df862254f54ea5019de907973c6c006267d28
josha <josha@LAPTOP-PN7OE63V>
2023-11-14 13:25:14 +0000

    A107 finished

---
+++ A107 Psuedo Code conversion/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A107_Psuedo_Code_conversion
+{
+    internal class Program
+    {
+        static void Q1() 
+        {
+            int PlayerOneScore = 0;
+            int PlayerTwoScore = 0;
+            Console.WriteLine("How many games?");
+            int NoOfGamesInMatch = int.Parse(Console.ReadLine());
+            for(int NoOfGamesPlayed = 1; NoOfGamesPlayed <= NoOfGamesInMatch; NoOfGamesPlayed++)
+            {
+                Console.WriteLine("Did Player One win the game (enter Y or N) ?");
+                char PlayerOneWinsGame = Console.ReadKey().KeyChar;
+                if (PlayerOneWinsGame == 'Y')
+                {
+                    PlayerOneScore++;
+                }
+                else
+                {
+                    PlayerTwoScore++;
+                }
+            }
+            Console.WriteLine(PlayerOneScore);
+            Console.WriteLine(PlayerTwoScore);
+        }
+
+        static void Q2()
+        {
+            Console.WriteLine("The first few prime numbers are:");
+            for (int Count1 = 2; Count1 < 51; Count1++)
+            {
+                int Count2 = 2;
+                string Prime = "Yes";
+                while(Count2 * Count2 <= Count1)
+                {
+                    if(Count1 % Count2 == 0)
+                    {
+                        Prime = "No";
+                    }
+                }
+                if (Prime == "Yes")
+                {
+                    Console.WriteLine(Count1);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Project would you like to run?");
+            string Prog_run;
+            do
+            {
+                Prog_run = Console.ReadLine();
+                switch (Prog_run)
+                {
+                    case "1": Q1(); break;
+                    case "2": Q2(); break;
+                    default: Console.WriteLine("Not an option"); break;
+                }
+            }while(Prog_run != "1" && Prog_run != "2");
+
+        }
+    }
+}
