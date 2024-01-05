dcb41248fb6f7be11694d20e80d1c33e796a88f6
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-01-05 14:11:33 +0000

    A113

---
+++ A113 LeapYear/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A113_LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what year to test?");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine((LeapYear(year)) ? $"{year} is a Leap Year" : $"{year} is not a Leap Year");
+            Console.ReadKey();
+        }
+
+        static bool LeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0) return true;
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
