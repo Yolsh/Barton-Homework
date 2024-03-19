bdbce9e0d160c25429a3c28bee0dc90c9207c811
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-03-19 13:48:22 +0000

    W132

---
+++ W132/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Drawable> shapes = new List<Drawable>();
+
+            shapes.Add(new Square(3));
+            shapes.Add(new Square(5));
+
+            shapes.Add(new VerticalLine(3));
+            shapes.Add(new VerticalLine(5));
+
+            shapes.Add(new Pyramid(5));
+            shapes.Add(new Pyramid(2));
+
+            shapes.Add(new HorizontalLine(3));
+            shapes.Add(new HorizontalLine(5));
+
+            foreach (Drawable shape in shapes)
+            {
+                shape.draw();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ W132/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W132
+{
+    public class Square : Drawable
+    {
+        private int width;
+
+        public Square(int side)
+        {
+            width = side;
+        }
+
+        public override void draw()
+        {
+            for (int y = 0; y < width; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
