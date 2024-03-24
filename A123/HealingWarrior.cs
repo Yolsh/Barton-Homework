df7d0a4497f2c64d6dfb15c4d5629f6466939e90
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-03-24 14:48:57 +0000

    A123 part 1

---
+++ A123/HealingWarrior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A123
+{
+    public class HealingWarrior : Warrior
+    {
+        private bool haveHealed;
+
+        public HealingWarrior(string myName) : base(myName)
+        {
+            haveHealed = false;
+        }
+
+        public bool getHaveHealed()
+        {
+            return haveHealed;
+        }
+
+        public void heal()
+        {
+            if (!haveHealed)
+            {
+                currentHealth = maxHealth;
+                haveHealed = true;
+            }
+            else
+            {
+                throw new NotSupportedException("Your warrior has already healed");
+            }
+        }
+    }
+}
---
+++ A123/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace A123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many sides should the dice have.");
+            Dice mydice = new Dice(int.Parse(Console.ReadLine()));
+
+            HealingWarrior hero = new HealingWarrior("Josh");
+            HealingWarrior enemy = new HealingWarrior("Joe");
+
+            Console.WriteLine($"{hero.GetName()} has a starting health of {hero.GetHealth()}");
+            Console.WriteLine($"{enemy.GetName()} has a starting health of {enemy.GetHealth()}\n");
+            Console.ReadKey();
+            Console.Clear();
+
+            while (hero.isAlive() && enemy.isAlive())
+            {
+                enemy.Attack(hero, mydice.Roll());
+                Console.WriteLine($"{enemy.GetName()} has a current health of {enemy.GetHealth()}\n");
+                Heal(enemy);
+                hero.Attack(enemy, mydice.Roll());
+                Console.WriteLine($"{hero.GetName()} has a current health of {hero.GetHealth()}\n");
+                Heal(hero);
+            }
+
+            Console.WriteLine((hero.isAlive()) ? $"{hero.GetName()} has won" : $"{enemy.GetName()} has won");
+            Console.ReadKey();
+        }
+
+        static void Heal(HealingWarrior warrior)
+        {
+            Console.WriteLine($"{warrior.GetName()} would you like to heal?");
+            string ans = Console.ReadLine();
+            if (ans.ToLower() == "yes")
+            {
+                try
+                {
+                    warrior.heal();
+                }
+                catch (NotSupportedException e)
+                {
+                    Console.WriteLine($"sorry {warrior.GetName()} {e.Message}");
+                    return;
+                }
+                Console.WriteLine($"{warrior.GetName()} has healed back to {warrior.GetHealth()}");
+            }
+        }
+    }
+}
