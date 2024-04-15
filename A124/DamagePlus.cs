2b5985cf9a5ec5dab4439cb7f6be6ccf1bd2f7a3
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-04-15 16:25:57 +0100

    A124

---
+++ A124/DamagePlus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A124
+{
+    public class DamagePlus : Item
+    {
+        public DamagePlus() 
+        {
+            Name = "D+";
+        }
+
+        public override void UseItem(Warrior UsedOn)
+        {
+            UsedOn.attackDamage *= 2;
+        }
+    }
+}
---
+++ A124/Dice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A124
+{
+    public class Dice
+    {
+        private int sidesCount;
+        private Random rand = new Random();
+
+        public Dice()
+        {
+            sidesCount = 6;
+        }
+
+        public Dice(int sides)
+        {
+            sidesCount = sides;
+        }
+
+        public int GetSideCount()
+        {
+            return sidesCount;
+        }
+
+        public int Roll()
+        {
+            return rand.Next(1, sidesCount + 1);
+        }
+    }
+}
