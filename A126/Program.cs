464c3436e076f15a12b687fea2eba74a5eaf0087
23JA1168 <23JA1168@L2290>
2024-04-24 13:14:16 +0100

    A126

---
+++ A126/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A126
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iterate: Num Pls?");
+            Console.WriteLine(DoubleFactIterate(int.Parse(Console.ReadLine())));
+            Console.WriteLine("Recursive: Num Pls?");
+            Console.WriteLine(DoubleFactIterate(int.Parse(Console.ReadLine())));
+            Console.ReadKey();
+        }
+
+        static int DoubleFactRec(int n) => n <= 0 ? 1 : n * DoubleFactRec(n - 2);
+
+        static int DoubleFactIterate(int num)
+        {
+            int OutNum = 1;
+            while (num > 0)
+            {
+                OutNum *= num;
+                num = num - 2;
+            }
+            return OutNum;
+        }
+    }
+}
