e11d2ea520ba7edcf48f0a0a06025582882f747f
23JA1168 <23JA1168@L2377>
2024-05-15 12:49:58 +0100

    A128

---
+++ A128/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A128
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DeCipher(Console.ReadLine()));
+            Console.ReadKey();
+        }
+
+        static string DeCipher(string n) => n.Length >= 2 ? 
+            int.Parse(n.Substring(0, 2)) <= 97 && int.Parse(n.Substring(0, 2)) != 32 ? 
+            n.Length >= 3 ?
+            int.Parse(n.Substring(0, 3)) >= 122 ? "" : 
+            ((char)int.Parse(n.Substring(0, 3))).ToString() + DeCipher(new string(n.Skip(3).ToArray())) : "" : 
+            ((char)int.Parse(n.Substring(0, 2))).ToString() + DeCipher(new string(n.Skip(2).ToArray())) : "";
+    }
+}
