6390057cd43529c6e144ccb6bf5f2dbc54c66be2
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-06-17 21:52:50 +0100

    A132

---
+++ A132/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Uncensor("xyz", ""));
+            Console.ReadKey();
+        }
+
+        static string Uncensor(string plaintext, string missingLetters)
+        {
+            string fullText = "";
+            int i = 0;
+            foreach (char c in plaintext)
+            {
+                if (c == '*')
+                {
+                    fullText += missingLetters[i];
+                    i++;
+                    continue;
+                }
+                fullText += c;
+            }
+            return fullText;
+        }
+    }
+}
