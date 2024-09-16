35d2de13f234efe683d599c45c0c9e32bb9b9d74
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-09-16 20:35:10 +0100

    A200

---
+++ A200/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace A200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>
+            {
+                "ABodE",
+                "abc",
+                "xyz",
+                "xBkd"
+            };
+            Console.WriteLine(MatchChecker(strings));
+            Console.ReadKey();
+        }
+
+        static string MatchChecker(List<string> s) => s.Select(x => x.ToLower()).Select(x => Enumerable.Range(0, 26).Aggregate(0, (tot, cur) => cur > x.Length - 1 ? tot : cur + 97 == x[cur] ? tot+1 : tot).ToString()).Aggregate("", (tot, next) => tot += $"{next}, ");
+    }
+}
