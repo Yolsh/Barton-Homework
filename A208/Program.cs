3e02058bcf581176bbb8efc9057a4bd918dd0ea0
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-11-18 10:00:13 +0000

    A208

---
+++ A208/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A208
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Feast("ron weasley", "raspberry trifle"));
+            Console.ReadKey();
+        }
+
+        static bool Feast(string student, string dish) => student.ToLower()[0] == dish.ToLower()[0] && student.ToLower()[student.Length-1] == dish.ToLower()[dish.Length-1];
+    }
+}
